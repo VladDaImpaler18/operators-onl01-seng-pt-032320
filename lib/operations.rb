387a109057d27d03e0d1f9108d0d9747126e4260
Vladimir J <vladdaimpaler@protonmail.com>
2020-03-26 20:59:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,9 +7,9 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 ? true
-	speed < 40 ? true
-	speed >= 40 && speed <=60 ? false
+	speed > 60 ? true:
+	speed < 40 ? true:
+	speed >= 40 && speed <=60 ? false:
 end
 	
 
